2399d739f6b410dd0c4280d6a97b0b05b117c3ba
Пятина Даша <dora628962@gmail.com>
2025-05-07 00:07:22 +0700

    Создание файла books.js

---
+++ lab6/books.js
@@ -0,0 +1,17 @@
+const books = [
+    {
+        author: 'Сара Дж. Маас',
+        name: 'Королевство пепла',
+        publicationYear: 2021
+    }, 
+    {
+        author: 'Халед Хоссейни',
+        name: 'Тысяча сияющих солнц',
+        publicationYear: 2015
+    }, 
+    {
+        author: 'Михаил Булгаков',
+        name: 'Мастер и Маргарита',
+        publicationYear: 1967
+    }, 
+]
