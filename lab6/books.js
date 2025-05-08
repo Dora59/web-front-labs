65b24d8a2105f33917dd966a71667cd0c7ebb08e
Пятина Даша <dora628962@gmail.com>
2025-05-08 23:37:33 +0700

    Добавление книг

--- lab6/books.js
+++ lab6/books.js
@@ -12,6 +12,41 @@ const books = [
     {
         author: 'Михаил Булгаков',
         name: 'Мастер и Маргарита',
-        publicationYear: 1967
+        publicationYear: 1967,
+    }, 
+    {
+        author: 'Олдос Хаксли',
+        name: 'О дивный новый мир',
+        publicationYear: 2017,
+    }, 
+    {
+        author: 'Паоло Коэльо',
+        name: 'Алхимик',
+        publicationYear:1988 ,
+    }, 
+    {
+        author: 'Р. Джей Паласио',
+        name: 'Чудо',
+        publicationYear: 2017,
+    }, 
+    {
+        author: 'Элизабет Гилберт',
+        name: 'Город женщин',
+        publicationYear: 2019,
+    }, 
+    {
+        author: 'Салли Руни',
+        name: 'Нормальные люди',
+        publicationYear: 2020,
+    }, 
+    {
+        author: '',
+        name: '',
+        publicationYear: ,
+    }, 
+    {
+        author: 'Чехов Антон',
+        name: 'Дама с собачкой',
+        publicationYear: 1899,
     }, 
 ]
