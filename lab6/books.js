002a21e4d4393a256ca531a83317918109b46932
Пятина Даша <dora628962@gmail.com>
2025-05-09 00:01:03 +0700

    Дополнительная информация о книгах

--- lab6/books.js
+++ lab6/books.js
@@ -2,51 +2,81 @@ const books = [
     {
         author: 'Сара Дж. Маас',
         name: 'Королевство пепла',
-        publicationYear: 2021
+        publicationYear: 2021,
+        price: 890,
+        bestseller: true,
+        available: true
     }, 
     {
         author: 'Халед Хоссейни',
         name: 'Тысяча сияющих солнц',
-        publicationYear: 2015
+        publicationYear: 2015, 
+        price: 559,
+        bestseller: true,
+        available: true
     }, 
     {
         author: 'Михаил Булгаков',
         name: 'Мастер и Маргарита',
         publicationYear: 1967,
+        price: 297,
+        bestseller: true,
+        available: true
     }, 
     {
         author: 'Олдос Хаксли',
         name: 'О дивный новый мир',
         publicationYear: 2017,
+        price: 479,
+        bestseller: false,
+        available: false
     }, 
     {
         author: 'Паоло Коэльо',
         name: 'Алхимик',
-        publicationYear:1988 ,
+        publicationYear:1988,
+        price: 986,
+        bestseller: true,
+        available: false
     }, 
     {
         author: 'Р. Джей Паласио',
         name: 'Чудо',
         publicationYear: 2017,
+        price: 958,
+        bestseller: true,
+        available: false
     }, 
     {
         author: 'Элизабет Гилберт',
         name: 'Город женщин',
         publicationYear: 2019,
+        price: 497,
+        bestseller: true,
+        available: true
     }, 
     {
         author: 'Салли Руни',
         name: 'Нормальные люди',
         publicationYear: 2020,
+        price: 929,
+        bestseller: true,
+        available: true
     }, 
     {
         author: 'Федор Михайлович Достоевский',
         name: 'Игрок',
         publicationYear:1866,
+        price: 303,
+        bestseller: true,
+        available: true
     }, 
     {
         author: 'Чехов Антон',
         name: 'Дама с собачкой',
         publicationYear: 1899,
+        price: 239,
+        bestseller: false,
+        available: true
     }, 
 ]
